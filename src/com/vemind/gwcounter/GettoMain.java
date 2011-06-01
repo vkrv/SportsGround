6be96e176bc2d2161110debccc01a62a0eb80fbf
Vadim Kravchenko <kravchenko@spb.gs.ru>
2011-06-01 19:00:31 +0400

    unnecessary code temporary commented, new options menu item added (non-functional yet)

--- src/com/vemind/gwcounter/GettoMain.java
+++ src/com/vemind/gwcounter/GettoMain.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -13,6 +12,7 @@ public class GettoMain extends Activity {
     private final int PROFILES_ID = Menu.FIRST;
 	private final  int SETTINGS_ID = PROFILES_ID + 1;
 	private final int EXIT_ID = SETTINGS_ID + 1;
+	private final int SAVE_ID = EXIT_ID + 1;
 
 	private GettoProfile currentProf;
 	private ProfileManager profManager;
@@ -20,8 +20,8 @@ public class GettoMain extends Activity {
 	private TextView textStatus;
 	private EditText dipsEdit;
 	private EditText pullsEdit;
-	private Button dipsButton;
-	private Button pullsButton;
+//	private Button dipsButton;
+//	private Button pullsButton;
 	
 	
 	/** Called when the activity is first created. */
@@ -32,8 +32,8 @@ public class GettoMain extends Activity {
         
         dipsEdit = (EditText) findViewById (R.id.dips_edit);
         pullsEdit = (EditText) findViewById (R.id.pulls_edit);
-        dipsButton = (Button) findViewById (R.id.dips_button);
-        pullsButton = (Button) findViewById (R.id.pulls_button);
+//        dipsButton = (Button) findViewById (R.id.dips_button);
+//        pullsButton = (Button) findViewById (R.id.pulls_button);
         textStatus = (TextView) findViewById (R.id.text_status);
         
         profManager = ProfileManager.getInstance();
@@ -50,6 +50,7 @@ public class GettoMain extends Activity {
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
     	menu.clear();
+        menu.add(0, SAVE_ID, 0, R.string.save_session);
         menu.add(0, PROFILES_ID, 0, R.string.switch_profiles);
         menu.add(0, SETTINGS_ID, 0, R.string.settings);
         menu.add(0, EXIT_ID, 0, R.string.exit);
@@ -63,6 +64,9 @@ public class GettoMain extends Activity {
                 switchProfiles();                
                 return true;
             case SETTINGS_ID:
+            	showSettings();
+            	return true;
+            case SAVE_ID:
             	showSettings();
             	return true;
             case EXIT_ID:
