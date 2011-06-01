1d9305428315599c09b287c36f55a21601156d32
Vadim Kravchenko <kravchenko@spb.gs.ru>
2011-06-01 18:36:52 +0400

    git structure updated

---
+++ src/com/vemind/gwcounter/SessionData.java
@@ -0,0 +1,16 @@
+package com.vemind.gwcounter;
+
+import java.util.Date;
+
+public class SessionData {
+	public Integer pulls;
+	public Integer dips;
+	public Date started;
+	
+	public SessionData () {
+		pulls = 0;
+		dips = 0;
+		started = null;
+	}
+
+}
